03d7c89f707820cbc7bcb5b31bf3dd401423a224
ldelbel <lucdelbel@gmail.com>
2021-02-17 12:06:42 -0300

    Add component Details and styling

--- src/API/api.js
+++ src/API/api.js
@@ -1,11 +1,21 @@
 const API_KEY = '?apikey=f25597924d4529b7e1d2e609c841a5aa';
 const PROFILE_URL = 'https://financialmodelingprep.com/api/v3/profile/';
 const LIST_URL = 'https://financialmodelingprep.com/api/v3/financial-statement-symbol-lists';
-const SECTOR_URL = 'https://financialmodelingprep.com/api/v3/sectors-performance';
+const QUOTE_URL = 'https://financialmodelingprep.com/api/v3/quote/';
 
 const profile = async company => {
   try {
     const response = await fetch(`${PROFILE_URL}${company}${API_KEY}`, { mode: 'cors' });
+    const result = await response.json();
+    return result[0];
+  } catch (err) {
+    return err;
+  }
+};
+
+const quote = async symbol => {
+  try {
+    const response = await fetch(`${QUOTE_URL}${symbol}${API_KEY}`, { mode: 'cors' });
     const result = await response.json();
     return result[0];
   } catch (err) {
@@ -23,14 +33,6 @@ const list = async () => {
   }
 };
 
-const sectors = async () => {
-  try {
-    const response = await fetch(`${SECTOR_URL}${API_KEY}`, { mode: 'cors' });
-    const result = await response.json();
-    return result.map(e => e.sector);
-  } catch (err) {
-    return err;
-  }
-};
 
-export { profile, list, sectors };
+
+export { profile, list, quote };
--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,12 +1,255 @@
 import React from 'react';
 import { useParams } from 'react-router';
+import Navbar from '../components/Navbar';
+import { quote } from '../API/api';
 
 const Details = () => {
   const { symbol } = useParams();
+  console.log(symbol);
   return (
-    <div>
-      { symbol }
-    </div>
+    <>
+      <Navbar />
+      <div className="container d-flex flex-row justify-content-between" style={{ marginTop: 30, height: '75vh', width: '95%' }}>
+        <div style={{
+          height: '100%', width: '69%', background: 'rgba(108,117,125,0.16)', borderRadius: 20, padding: 20,
+        }}
+        >
+          <div
+            className="d-flex flex-row justify-content-between align-items-center"
+            style={{
+              height: '15%', width: '100%', fontSize: 40, fontFamily: '"Alfa Slab One", cursive', borderStyle: 'none', borderBottomStyle: 'solid',
+            }}
+          >
+            <span style={{ fontSize: 50, height: 60 }}>AAPL</span>
+          </div>
+          <div style={{ height: '5%', marginBottom: 20 }}><span style={{ color: 'var(--blue)', fontFamily: '"Alfa Slab One", cursive' }}>NASDAQ</span></div>
+          <div className="d-flex flex-row" style={{ height: '80%' }}>
+            <div style={{
+              background: 'var(--dark)', width: '61%', height: '90%', borderRadius: 20, padding: 15,
+            }}
+            >
+              <div className="d-flex flex-row justify-content-between">
+                <span style={{
+                  fontSize: 20, fontFamily: 'Alata, sans-serif', fontWeight: 'bold', marginRight: 0, color: 'var(--light)',
+                }}
+                >
+                  MktCap:
+                </span>
+                <span
+                  className="d-lg-flex align-items-lg-end"
+                  style={{
+                    fontSize: 18, fontFamily: 'Alata, sans-serif', color: 'var(--success)', borderTopColor: 'var(--green)',
+                  }}
+                >
+                  U$&nbsp;2.068.718.419.968.00
+                </span>
+              </div>
+              <div className="d-flex flex-row justify-content-between">
+                <span style={{
+                  fontSize: 20, fontFamily: 'Alata, sans-serif', fontWeight: 'bold', marginRight: 20, color: 'var(--light)',
+                }}
+                >
+                  SharesOut:
+                </span>
+                <span style={{ fontSize: 18, fontFamily: 'Alata, sans-serif', color: 'var(--blue)' }}>17102500165</span>
+              </div>
+              <div className="d-flex flex-row justify-content-center" style={{ borderTopStyle: 'solid', borderTopColor: 'var(--light)' }}>
+                <span style={{
+                  fontSize: 20, fontFamily: 'Alata, sans-serif', fontWeight: 'bold', marginRight: 20, marginTop: 15, marginBottom: 15, fontStyle: 'italic', color: 'var(--light)',
+                }}
+                >
+                  Transactions
+                </span>
+              </div>
+              <div className="d-flex flex-row justify-content-between">
+                <span style={{
+                  fontSize: 20, fontFamily: 'Alata, sans-serif', fontWeight: 'bold', marginRight: 20, color: 'var(--light)',
+                }}
+                >
+                  Volume:
+                </span>
+                <span style={{ fontSize: 18, fontFamily: 'Alata, sans-serif', color: 'var(--blue)' }}>332607163</span>
+              </div>
+              <div className="d-flex flex-row justify-content-between">
+                <span style={{
+                  fontSize: 20, fontFamily: 'Alata, sans-serif', fontWeight: 'bold', marginRight: 20, color: 'var(--light)',
+                }}
+                >
+                  AvgVolume:
+                </span>
+                <span style={{ fontSize: 18, fontFamily: 'Alata, sans-serif', color: 'var(--blue)' }}>165778904</span>
+              </div>
+              <div className="d-flex flex-row justify-content-between">
+                <span style={{
+                  fontSize: 20, fontFamily: 'Alata, sans-serif', fontWeight: 'bold', marginRight: 20, color: 'var(--light)',
+                }}
+                >
+                  PriceAvg50:
+                </span>
+                <span style={{ fontSize: 18, fontFamily: 'Alata, sans-serif', color: 'var(--success)' }}>U$&nbsp;112.15</span>
+              </div>
+              <div className="d-flex flex-row justify-content-between">
+                <span style={{
+                  fontSize: 20, fontFamily: 'Alata, sans-serif', fontWeight: 'bold', marginRight: 20, color: 'var(--light)',
+                }}
+                >
+                  PriceAvg200:
+                </span>
+                <span style={{ fontSize: 18, fontFamily: 'Alata, sans-serif', color: 'var(--success)' }}>U$&nbsp;112.15</span>
+              </div>
+            </div>
+            <div style={{
+              height: '100%', width: '39%', padding: 15, paddingTop: 0, paddingLeft: 25,
+            }}
+            >
+              <div className="d-flex flex-row justify-content-start align-items-center">
+                <span style={{
+                  fontFamily: 'Alata, sans-serif', fontSize: 40, height: 50, margin: '-5px',
+                }}
+                >
+                  Price
+                </span>
+              </div>
+              <div className="d-flex flex-row justify-content-start align-items-center" style={{ height: '20%' }}>
+                <div className="d-flex" style={{ height: 40, borderTopStyle: 'none' }}>
+                  <span
+                    className="d-flex flex-row justify-content-center align-items-center"
+                    style={{
+                      fontFamily: 'Aleo, serif', color: 'rgb(255,214,0)', fontSize: 38, fontWeight: 'bold', textShadow: '1px 1px 2px rgb(0,0,0)',
+                    }}
+                  >
+                    U$ 1202.03
+                  </span>
+                </div>
+              </div>
+              <div className="d-flex flex-row justify-content-start align-items-center" style={{ marginTop: 20 }}>
+                <span style={{
+                  fontFamily: 'Alata, sans-serif', fontSize: 25, height: 30, margin: '-5px',
+                }}
+                >
+                  Open
+                </span>
+              </div>
+              <div className="d-flex flex-row justify-content-start align-items-center" style={{ height: '20%' }}>
+                <div className="d-flex" style={{ height: 40, borderTopStyle: 'none' }}>
+                  <span
+                    className="d-flex flex-row justify-content-center align-items-center"
+                    style={{
+                      fontFamily: 'Alata, sans-serif', color: 'var(--green)', fontSize: 28, fontWeight: 'bold',
+                    }}
+                  >
+                    U$ 1202.03
+                  </span>
+                </div>
+              </div>
+              <div className="d-flex flex-row justify-content-start align-items-center">
+                <span style={{
+                  fontFamily: 'Alata, sans-serif', fontSize: 25, height: 30, margin: '-5px',
+                }}
+                >
+                  Previous Closed
+                </span>
+              </div>
+              <div className="d-flex flex-row justify-content-start align-items-center" style={{ height: '20%' }}>
+                <div className="d-flex" style={{ height: 40, borderTopStyle: 'none' }}>
+                  <span
+                    className="d-flex flex-row justify-content-center align-items-center"
+                    style={{
+                      fontFamily: 'Alata, sans-serif', color: 'var(--danger)', fontSize: 28, fontWeight: 'bold',
+                    }}
+                  >
+                    U$ 1202.03
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div style={{
+          height: '100%', width: '30%', background: 'var(--dark)', borderRadius: 20, padding: 20, paddingTop: 10,
+        }}
+        >
+          <div className="d-flex justify-content-center" style={{ marginBottom: 15 }}>
+            <span style={{
+              fontSize: 36, fontFamily: 'Alata, sans-serif', color: 'var(--light)', fontWeight: 'bold', fontStyle: 'italic',
+            }}
+            >
+              Quote
+            </span>
+          </div>
+          <div className="d-flex flex-column align-items-end">
+            <div style={{
+              borderBottomStyle: 'solid', borderBottomColor: 'var(--light)', height: 30, width: '100%',
+            }}
+            >
+              <span style={{ color: 'var(--light)', fontSize: 20, fontFamily: 'Alata, sans-serif' }}>Change (%)</span>
+            </div>
+            <span style={{
+              color: 'var(--yellow)', fontSize: 28, fontFamily: 'Alata, sans-serif', fontWeight: 'bold', margin: '-5px',
+            }}
+            >
+              0.07
+            </span>
+          </div>
+          <div className="d-flex flex-column align-items-end">
+            <div style={{
+              borderBottomStyle: 'solid', borderBottomColor: 'var(--light)', height: 30, width: '100%',
+            }}
+            >
+              <span style={{ color: 'var(--light)', fontSize: 20, fontFamily: 'Alata, sans-serif' }}>Day High (U$)</span>
+            </div>
+            <span style={{
+              color: 'var(--green)', fontSize: 28, fontFamily: 'Alata, sans-serif', fontWeight: 'bold', margin: '-5px',
+            }}
+            >
+              123.02
+            </span>
+          </div>
+          <div className="d-flex flex-column align-items-end">
+            <div style={{
+              borderBottomStyle: 'solid', borderBottomColor: 'var(--light)', height: 30, width: '100%',
+            }}
+            >
+              <span style={{ color: 'var(--light)', fontSize: 20, fontFamily: 'Alata, sans-serif' }}>Day Low (U$)</span>
+            </div>
+            <span style={{
+              color: 'var(--red)', fontSize: 28, fontFamily: 'Alata, sans-serif', fontWeight: 'bold', margin: '-5px',
+            }}
+            >
+              110.98
+            </span>
+          </div>
+          <div className="d-flex flex-column align-items-end">
+            <div style={{
+              borderBottomStyle: 'solid', borderBottomColor: 'var(--light)', height: 30, width: '100%',
+            }}
+            >
+              <span style={{ color: 'var(--light)', fontSize: 20, fontFamily: 'Alata, sans-serif' }}>Year High&nbsp; (U$)</span>
+            </div>
+            <span style={{
+              color: 'var(--green)', fontSize: 28, fontFamily: 'Alata, sans-serif', fontWeight: 'bold', margin: '-5px',
+            }}
+            >
+              <strong>123.02</strong>
+            </span>
+          </div>
+          <div className="d-flex flex-column align-items-end">
+            <div style={{
+              borderBottomStyle: 'solid', borderBottomColor: 'var(--light)', height: 30, width: '100%',
+            }}
+            >
+              <span style={{ color: 'var(--light)', fontSize: 20, fontFamily: 'Alata, sans-serif' }}>Year Low (U$)</span>
+            </div>
+            <span style={{
+              color: 'var(--red)', fontSize: 28, fontFamily: 'Alata, sans-serif', fontWeight: 'bold', margin: '-5px',
+            }}
+            >
+              <strong>110.98</strong>
+            </span>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
